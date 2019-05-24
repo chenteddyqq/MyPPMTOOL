0d81aae62e13a99d10e3276529990dd3f1dbe9a6
chenteddyqq <chenteddy@outlook.com>
2019-05-24 23:46:28 +0800

    first commit

---
+++ MyPPMTOOL/src/main/java/com/mytedted/ppmtool/MyPpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.mytedted.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPpmtoolApplication.class, args);
+	}
+
+}
